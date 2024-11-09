eb05b6224bad0978846969ad23cb1a615fc0836c
akashgajjala01 <akashgajjala01@gmail.com>
2024-11-09 17:01:22 +1030

    Task 1.4P basic programming

---
+++ Task 1.4P/Program.py
@@ -0,0 +1,12 @@
+print("Welcome to SIT725")
+
+number1 = int(input("N1 : "))
+number2 = int(input("N2 : "))
+
+# Addition
+sum_r = number1 + number2
+print(f"sum : {sum_r}")
+
+# Multiplication
+product_r= number1 * number2
+print(f"Product : {product_r}")
